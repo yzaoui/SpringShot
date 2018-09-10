6f0b1d8b69dd1f85a4f2075faef32dcfefd65b24
Yann Zaoui <yzaoui7@gmail.com>
2018-09-09 21:49:59 -0400

    Implement basic keyboard movement

---
+++ core/src/main/kotlin/MainScreen.kt
@@ -0,0 +1,151 @@
+package com.bitwiserain.core
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.InputAdapter
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import ktx.app.KtxScreen
+
+private const val TIMESTEP: Float = .01f
+
+class MainScreen : KtxScreen {
+    val batch = SpriteBatch()
+    val img = Texture("player.png")
+    val char = Character()
+    var accumulator = 0f
+
+    init {
+        Gdx.input.inputProcessor = object : InputAdapter() {
+            override fun keyDown(keycode: Int): Boolean {
+                when (keycode) {
+                    Input.Keys.LEFT, Input.Keys.A -> char.pressLeft()
+                    Input.Keys.RIGHT, Input.Keys.D -> char.pressRight()
+                    Input.Keys.UP, Input.Keys.W -> char.jump()
+                    else -> return super.keyDown(keycode)
+                }
+
+                return true
+            }
+
+            override fun keyUp(keycode: Int): Boolean {
+                when (keycode) {
+                    Input.Keys.LEFT, Input.Keys.A -> char.releaseLeft()
+                    Input.Keys.RIGHT, Input.Keys.D -> char.releaseRight()
+                    else -> return super.keyUp(keycode)
+                }
+
+                return true
+            }
+        }
+    }
+
+    override fun render(delta: Float) {
+        accumulator += delta
+
+        while (accumulator >= TIMESTEP) {
+            update()
+            accumulator -= TIMESTEP
+        }
+
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        batch.begin()
+
+        batch.draw(img, char.pos.x, char.pos.y)
+
+        batch.end()
+    }
+
+    fun update() {
+        char.step()
+    }
+
+    override fun dispose() {
+        batch.dispose()
+        img.dispose()
+    }
+}
+
+enum class HorizontalState {
+    IDLE,
+    MOVING,
+    MOVING_CANCELLED
+}
+
+enum class VerticalState {
+    GROUND,
+    AIR
+}
+
+enum class Facing {
+    LEFT,
+    RIGHT
+}
+
+private val GRAVITY = Vector2(0f, -0.3f)
+
+class Character(val pos: Vector2 = Vector2(100f, 0f)) {
+    private var horizontalState = HorizontalState.IDLE
+    private var facing = Facing.RIGHT
+    private val vel = Vector2(3f, 0f)
+    private var verticalState = VerticalState.GROUND
+
+    fun pressLeft() {
+        if (horizontalState == HorizontalState.MOVING && facing == Facing.RIGHT) horizontalState = HorizontalState.MOVING_CANCELLED
+        else if (horizontalState == HorizontalState.IDLE) horizontalState = HorizontalState.MOVING
+        facing = Facing.LEFT
+    }
+
+    fun releaseLeft() {
+        if (horizontalState == HorizontalState.MOVING) {
+            horizontalState = HorizontalState.IDLE
+        } else if (horizontalState == HorizontalState.MOVING_CANCELLED) {
+            horizontalState = HorizontalState.MOVING
+            facing = Facing.RIGHT
+        }
+    }
+
+    fun pressRight() {
+        if (horizontalState == HorizontalState.MOVING && facing == Facing.LEFT) horizontalState = HorizontalState.MOVING_CANCELLED
+        else if (horizontalState == HorizontalState.IDLE) horizontalState = HorizontalState.MOVING
+        facing = Facing.RIGHT
+    }
+
+    fun releaseRight() {
+        if (horizontalState == HorizontalState.MOVING) {
+            horizontalState = HorizontalState.IDLE
+        } else if (horizontalState == HorizontalState.MOVING_CANCELLED) {
+            horizontalState = HorizontalState.MOVING
+            facing = Facing.LEFT
+        }
+    }
+
+    fun jump() {
+        if (verticalState == VerticalState.GROUND) {
+            vel.y += 10f
+        }
+    }
+
+    fun step() {
+        vel.add(GRAVITY)
+
+        if (horizontalState == HorizontalState.MOVING) {
+            pos.x += when(facing) {
+                Facing.LEFT -> -vel.x
+                Facing.RIGHT -> vel.x
+            }
+        }
+
+        if (pos.y + vel.y <= 0) {
+            pos.y = 0f
+            vel.y = 0f
+        } else {
+            pos.y += vel.y
+        }
+
+        verticalState = if (pos.y > 0) VerticalState.AIR else VerticalState.GROUND
+    }
+}
