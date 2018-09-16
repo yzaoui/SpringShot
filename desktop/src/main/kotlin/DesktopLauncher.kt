18ad7b49a4cc95135323f8383a46c3b23ed3586b
Yann Zaoui <yzaoui7@gmail.com>
2018-09-15 20:36:22 -0400

    Add desktop icons, desktop title

--- desktop/src/main/kotlin/DesktopLauncher.kt
+++ desktop/src/main/kotlin/DesktopLauncher.kt
@@ -1,5 +1,6 @@
 package com.bitwiserain.desktop
 
+import com.badlogic.gdx.Files
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
 import com.bitwiserain.core.Application
@@ -7,6 +8,11 @@ import com.bitwiserain.core.Application
 object DesktopLauncher {
     @JvmStatic
     fun main(arg: Array<String>) {
-        LwjglApplication(Application(), LwjglApplicationConfiguration())
+        LwjglApplication(Application(), LwjglApplicationConfiguration().apply {
+            title = "SpringShot"
+            addIcon("icon-128.png", Files.FileType.Internal)
+            addIcon("icon-32.png", Files.FileType.Internal)
+            addIcon("icon-16.png", Files.FileType.Internal)
+        })
     }
 }
